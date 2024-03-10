2eaf89442bd7ba5ed2df729164747eb7d1e39204
Anjas amar pradana <anjastech01@gmail.com>
2024-03-10 12:54:17 +0700

    Update active-v2.sh

--- active-v2.sh
+++ active-v2.sh
@@ -13,7 +13,7 @@ chmod 600 /usr/local/directadmin/conf/license.key
 chown diradmin:diradmin /usr/local/directadmin/conf/license.key
 ifconfig eth0:100 103.187.146.38 netmask 255.0.0.0 up
 echo 'DEVICE=eth0:100' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
-echo 'IPADDR=10.48.114.194' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
+echo 'IPADDR=103.187.146.38' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
 echo 'NETMASK=255.0.0.0' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
 service network restart
 /usr/bin/perl -pi -e 's/^ethernet_dev=.*/ethernet_dev=eth0:100/' /usr/local/directadmin/conf/directadmin.conf
